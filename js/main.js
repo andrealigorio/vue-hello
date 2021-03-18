71c931cd1fa71c3ef4dc03842a8430c563909625
Andrea Ligorio <andrea85kro@gmail.com>
2021-03-18 14:31:14 +0100

    Esercizio completato

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            text: "Hello Vue",
+            img: "vuejs.png",
+            centered: "center"
+        }
+    }
+);
